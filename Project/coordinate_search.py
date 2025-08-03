e6015493b5fc2e65c963c2e3ea8392c97964fb6b
Toushal Sampat <toushal37@gmail.com>
2025-08-03 13:37:46 +0400

    commited a pushing

--- Project/coordinate_search.py
+++ Project/coordinate_search.py
@@ -1,3 +1,39 @@
-coordinate = (0, 0, 0)
-def get_coordinate():
-    return coordinate
+import random as r
+import time
+guess = []
+start_time = time.perf_counter()
+coordinates = (r.randint(0,100) , r.randint(0,100), r.randint(0,100))
+x = r.randint(0,100)
+y = r.randint(0,100)
+z = r.randint(0,100)
+guesses = 0
+while x != coordinates[0]:
+    if x > coordinates[0]:
+        x -= 1
+        guesses += 1
+    else:
+        x += 1
+        guesses += 1
+while y != coordinates[1]:
+    if y > coordinates[1]:
+        y -= 1
+        guesses += 1
+    else:
+        y += 1
+        guesses += 1
+while z != coordinates[2]:
+    if z > coordinates[2]:
+        z -= 1
+        guesses += 1
+    else:
+        z += 1
+        guesses += 1
+guess.append(guesses)
+end_time = time.perf_counter()
+runtime = end_time - start_time
+print(guess)
+sum = 0
+for a in guess:
+    sum += a
+print(sum/len(guess))
+print(f"Execution time: {runtime:.6f} seconds")
